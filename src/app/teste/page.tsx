6aa5a437a796d820bef97d2f4796387af8039862
Tonny221 <anthonny14gabriell@gmail.com>
2023-05-23 14:24:19 -0400

    testing functionalities

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <>
+      <h1 className='text-3xl text-violet-400'>Você não é real...</h1>
+    </>
+  )
+}
+
+export default page
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+    return (
+        <header className='p-4 mx-40 flex justify-between'>
+            <h1>
+                <Link href={'/'}>Nome do site</Link>
+            </h1>
+            <menu>
+                <ul className='flex justify-between gap-10'>
+                    <li>
+                        <Link href={"/teste"} className='hover:text-violet-400 transition delay-100 ease-in-out'>teste</Link>
+                    </li>
+                    <li>
+                        <Link href={"/galeria"} className='hover:text-violet-400 transition delay-100 ease-in-out'>galeria</Link>
+                    </li>
+                </ul>
+            </menu>
+            <aside>
+                <ul className='flex justify-between'>
+                    <li>
+                        <Link href={"/teste"}>teste</Link>
+                    </li>
+                    <li>
+                        <Link href={"/galeria"}>galeria</Link>
+                    </li>
+                </ul>
+            </aside>
+        </header>
+    )
+}
+
+export default Header
